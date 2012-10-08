c6a55855b110b02dc8c79102c703559c57018970
huangyifeng <luckyJerry85@gmail.com>
2012-10-08 18:45:12 +0800

    add launcher view target

---
+++ LauncherView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LauncherView
+//
+//  Created by HuangYiFeng on 10/8/12.
+//  Copyright (c) 2012 Cybozu, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LauncherView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LauncherView
+//
+//  Created by HuangYiFeng on 10/8/12.
+//  Copyright (c) 2012 Cybozu, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
